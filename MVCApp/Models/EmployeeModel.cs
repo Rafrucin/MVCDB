12d93c0810ce92c3832b1dae63d955f2c4b82d6a
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-02-28 23:37:45 +0000

    ok

--- MVCApp/Models/EmployeeModel.cs
+++ MVCApp/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,37 @@ namespace MVCApp.Models
 {
     public class EmployeeModel
     {
+        [Display(Name = "Employee ID")]
+        [Range(100000, 999999, ErrorMessage = "Enter valid employee number")]
         public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="Please entre your first name")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Please entre your Last name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Please entre your email address")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [DataType(DataType.EmailAddress)]
+        [Compare("EmailAddress", ErrorMessage ="Email Address and Confirm Email must match")]
         public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please entre your password")]
+        [StringLength(100, MinimumLength =10, ErrorMessage ="Password must be 10-100 characters")]
         public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password and confirm password must match")]
         public string ConfirmPassword { get; set; }
         
     }
